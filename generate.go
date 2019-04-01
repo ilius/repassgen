886beba2b6053dc5020540727c77d7e1f8667b96
Saeed Rasooli <saeed.gnu@gmail.com>
2019-04-01 20:47:46 +0430

    move code from main() to Generate(pattern)

---
+++ generate.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+// Generate generates random password based on given pattern
+// see README.md for examples of pattern
+func Generate(pattern string) []rune {
+	s := NewState(pattern)
+	lex := LexRoot
+	var err error
+	for {
+		lex, err = lex(s)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(fmt.Errorf("invalid syntax near character %d: %v", s.patternPos, err))
+		}
+	}
+	return s.output
+}
--- main.go
+++ main.go
@@ -2,23 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
 func main() {
 	pattern := os.Args[1]
-	s := NewState(pattern)
-	lex := LexRoot
-	var err error
-	for {
-		lex, err = lex(s)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			panic(fmt.Errorf("invalid syntax near character %d: %v", s.patternPos, err))
-		}
-	}
-	fmt.Println(string(s.output))
+	fmt.Println(string(Generate(pattern)))
 }
