5f9fdbcdd192bf0f879ae911a6a6fc59a0c50e63
Saeed Rasooli <saeed.gnu@gmail.com>
2019-04-02 09:09:35 +0430

    fix duplicate password when run twice in a second, avoid timestamp for seed Thanks to John Leidegren for this answer: https://stackoverflow.com/a/54491783/495838

--- init.go
+++ init.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"math/rand"
-	"time"
+	crypto_rand "crypto/rand"
+	"encoding/binary"
+	math_rand "math/rand"
 )
 
 func init() {
-	rand.Seed(time.Now().Unix())
+	var b [8]byte
+	_, err := crypto_rand.Read(b[:])
+	if err != nil {
+		panic("cannot seed math/rand package with cryptographically secure random number generator")
+	}
+	math_rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
 }
