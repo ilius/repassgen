d45f871d215c9bf310fba4a126f61be2e631665a
Saeed Rasooli <saeed.gnu@gmail.com>
2020-01-05 20:53:14 +0330

    functions.go: break down some long lines

--- functions.go
+++ functions.go
@@ -28,7 +28,11 @@ var encoderFunctions = map[string]func(in []rune) ([]rune, error){
 
 	// standard Base32 encode function (uppercase, with no padding)
 	"base32std": func(in []rune) ([]rune, error) {
-		return []rune(base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString([]byte(string(in)))), nil
+		return []rune(
+			base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(
+				[]byte(string(in)),
+			),
+		), nil
 	},
 
 	// Hex encode functions (lowercase and uppercase)
@@ -36,12 +40,14 @@ var encoderFunctions = map[string]func(in []rune) ([]rune, error){
 		return []rune(hex.EncodeToString([]byte(string(in)))), nil
 	},
 	"HEX": func(in []rune) ([]rune, error) {
-		return []rune(strings.ToUpper(hex.EncodeToString([]byte(string(in))))), nil
+		return []rune(strings.ToUpper(
+			hex.EncodeToString([]byte(string(in))),
+		)), nil
 	},
 
 	// Escape unicode characters, non-printable characters and double quote
-	// The returned string uses Go escape sequences (\t, \n, \xFF, \u0100) for non-ASCII
-	// characters and non-printable characters
+	// The returned string uses Go escape sequences (\t, \n, \xFF, \u0100)
+	// for non-ASCII characters and non-printable characters
 	"escape": func(in []rune) ([]rune, error) {
 		q := strconv.QuoteToASCII(string(in))
 		return []rune(q[1 : len(q)-1]), nil
