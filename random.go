bee7e6a7cfc1ffaad8f1b5eaff136ffecbdf20f3
Saeed Rasooli <saeed.gnu@gmail.com>
2020-01-14 19:50:04 +0330

    fix lint errors in random.go

--- random.go
+++ random.go
@@ -12,11 +12,11 @@ func NewRandSource() *math_rand.Rand {
 	return math_rand.New(CryptoRandSource{})
 }
 
-func (_ CryptoRandSource) Int63() int64 {
+func (CryptoRandSource) Int63() int64 {
 	var b [8]byte
 	rand.Read(b[:])
 	// mask off sign bit to ensure positive number
 	return int64(binary.LittleEndian.Uint64(b[:]) & (1<<63 - 1))
 }
 
-func (_ CryptoRandSource) Seed(_ int64) {}
+func (CryptoRandSource) Seed(_ int64) {}
