1badd476d5ca05fcddbbeb8dfb820e8e6b46b82a
Saeed Rasooli <saeed.gnu@gmail.com>
2020-01-17 17:50:03 +0330

    repeat_test.go: add test by groupGenerator as child, coverage: 86.1

--- repeat_test.go
+++ repeat_test.go
@@ -7,7 +7,32 @@ import (
 	"github.com/ilius/is"
 )
 
-func TestRepeatGenerator(t *testing.T) {
+func TestRepeatGeneratorByGroup(t *testing.T) {
+	is := is.New(t)
+	count := 4
+	pattern := fmt.Sprintf("[a-c]{%d}", count)
+	g := &repeatGenerator{
+		child: newGroupGenerator("[a-c]"),
+		count: count,
+	}
+	s := NewState(&SharedState{}, pattern)
+	{
+		entropy, err := g.Entropy()
+		is.ErrMsg(err, "entropy is not calculated")
+		is.Equal(0, entropy)
+	}
+	{
+		err := g.Generate(s)
+		is.NotErr(err)
+	}
+	{
+		entropy, err := g.Entropy()
+		is.NotErr(err)
+		isFloatBetween(is, entropy, 25.3, 25.4)
+	}
+}
+
+func TestRepeatGeneratorByStatic(t *testing.T) {
 	is := is.New(t)
 	str := "a"
 	count := 4
