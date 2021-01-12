4eb0c0f3e7af5c08412fa1988674c8df464fcd5b
Saeed Rasooli <saeed.gnu@gmail.com>
2021-01-12 19:14:37 +0330

    add rootgen_test.go to fully cover rootgen.go

---
+++ rootgen_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ilius/is/v2"
+)
+
+func TestRootGenerator(t *testing.T) {
+	is := is.New(t)
+	pattern := []rune("[a-z]{5}[1-9]{2}")
+	g := NewRootGenerator()
+	s := newTestState(pattern)
+	{
+		entropy, err := g.Entropy(s)
+		is.ErrMsg(err, "unknown error near index 15: entropy is not calculated")
+		is.Equal(0.0, entropy)
+	}
+	{
+		err := g.Generate(s)
+		is.NotErr(err)
+	}
+	{
+		entropy, err := g.Entropy(s)
+		is.NotErr(err)
+		isFloatBetween(is, entropy, 29.8, 29.9)
+	}
+}
