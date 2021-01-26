f511633e5c076abac5e9c5b8614d04cba677b99c
Saeed Rasooli <saeed.gnu@gmail.com>
2021-01-26 09:32:57 +0330

    refactoring lex_group.go

--- lex_group.go
+++ lex_group.go
@@ -11,45 +11,53 @@ func lexGroup(s *State) (LexType, error) {
 	s.move(1)
 	switch c {
 	case '\\':
-		if s.end() {
-			s.errorOffset++
-			return nil, s.errorSyntax("'(' not closed")
-		}
-		c := s.pattern[s.patternPos]
-		s.move(1)
-		s.patternBuff = append(s.patternBuff, '\\', c)
-		return lexGroup, nil
+		return processGroupBackslash(s)
 	case '(':
 		s.openParenth++
-		//s.lastGroupId++
 	case ')':
-		s.openParenth--
-		if s.openParenth > 0 {
-			break
-		}
-		groupId := s.lastGroupId
-		lastOutputSize := len(s.output)
-		s2 := NewState(&SharedState{}, s.pattern)
-		s2.output = s.output
-		s2.absPos = s.absPos - uint(len(s.patternBuff)) - 1
-		s2.patternEntropy = s.patternEntropy
-		s2.lastGroupId = groupId
-		s2.groupsOutput = s.groupsOutput
-		gen := newGroupGenerator(s.patternBuff)
-		err := gen.Generate(s2)
-		if err != nil {
-			return nil, err
-		}
-		s.output = s2.output
-		s.patternEntropy = s2.patternEntropy
-		s.lastGroupId = s2.lastGroupId
-		s.groupsOutput[groupId] = s.output[lastOutputSize:]
-		s.lastGen = gen
-		s.patternBuff = nil
-		return LexRoot, nil
+		return processGroupEnd(s)
 	}
 	s.patternBuff = append(s.patternBuff, c)
 	return lexGroup, nil
+}
+
+func processGroupBackslash(s *State) (LexType, error) {
+	if s.end() {
+		s.errorOffset++
+		return nil, s.errorSyntax("'(' not closed")
+	}
+	c := s.pattern[s.patternPos]
+	s.move(1)
+	s.patternBuff = append(s.patternBuff, '\\', c)
+	return lexGroup, nil
+}
+
+func processGroupEnd(s *State) (LexType, error) {
+	s.openParenth--
+	if s.openParenth > 0 {
+		s.patternBuff = append(s.patternBuff, ')')
+		return lexGroup, nil
+	}
+	groupId := s.lastGroupId
+	lastOutputSize := len(s.output)
+	s2 := NewState(&SharedState{}, s.pattern)
+	s2.output = s.output
+	s2.absPos = s.absPos - uint(len(s.patternBuff)) - 1
+	s2.patternEntropy = s.patternEntropy
+	s2.lastGroupId = groupId
+	s2.groupsOutput = s.groupsOutput
+	gen := newGroupGenerator(s.patternBuff)
+	err := gen.Generate(s2)
+	if err != nil {
+		return nil, err
+	}
+	s.output = s2.output
+	s.patternEntropy = s2.patternEntropy
+	s.lastGroupId = s2.lastGroupId
+	s.groupsOutput[groupId] = s.output[lastOutputSize:]
+	s.lastGen = gen
+	s.patternBuff = nil
+	return LexRoot, nil
 }
 
 func processGroupRef(s *State, parentLex LexType) (LexType, error) {
