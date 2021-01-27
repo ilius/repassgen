9f4fa24b33965b0c480643965b7bc8df344f7a0a
Saeed Rasooli <saeed.gnu@gmail.com>
2021-01-27 03:51:25 +0330

    add alter_test.go

---
+++ alter_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/ilius/is/v2"
+)
+
+func TestAlterGenerator(t *testing.T) {
+	is := is.New(t)
+	pattern := []rune("(a|bc)")
+	g := &alterGenerator{
+		parts:     [][]rune{[]rune("a"), []rune("bc")},
+		indexList: []int{0, 1},
+		absPos:    0,
+	}
+	s := newTestState(pattern)
+	{
+		entropy, err := g.Entropy(s)
+		is.ErrMsg(err, "unknown error near index 0: entropy is not calculated")
+		is.Equal(0.0, entropy)
+	}
+	{
+		err := g.Generate(s)
+		is.NotErr(err)
+	}
+	{
+		entropy, err := g.Entropy(s)
+		is.NotErr(err)
+		isFloatBetween(is, entropy, 1, 1)
+	}
+}
