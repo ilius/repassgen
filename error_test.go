e05ced1737cc752828dd69ff0c229c2320fd6dab
Saeed Rasooli <saeed.gnu@gmail.com>
2021-01-27 03:54:31 +0330

    error_test.go: test err.SpacedError()

--- error_test.go
+++ error_test.go
@@ -11,22 +11,26 @@ func TestError(t *testing.T) {
 	{
 		err := NewError(ErrorSyntax, 5, "dummy error")
 		is.ErrMsg(err, "syntax error near index 5: dummy error")
+		is.Equal(err.SpacedError(), `     ^ syntax error: dummy error`)
 	}
 	{
 		err := NewError(ErrorSyntax, 5, "dummy error")
 		err.AppendMsg("msg 2")
 		is.ErrMsg(err, "syntax error near index 5: dummy error: msg 2")
+		is.Equal(err.SpacedError(), `     ^ syntax error: dummy error: msg 2`)
 	}
 	{
 		err := NewError(ErrorSyntax, 5, "dummy error")
 		err.AppendMsg("msg 2")
 		err.PrependMsg("msg 3")
 		is.ErrMsg(err, "syntax error near index 5: msg 3: dummy error: msg 2")
+		is.Equal(err.SpacedError(), `     ^ syntax error: msg 3: dummy error: msg 2`)
 	}
 	{
 		err := NewError(ErrorUnknown, 5, "dummy error")
 		err.AppendMsg("msg 2")
 		err.PrependMsg("msg 3")
 		is.ErrMsg(err, "unknown error near index 5: msg 3: dummy error: msg 2")
+		is.Equal(err.SpacedError(), `     ^ unknown error: msg 3: dummy error: msg 2`)
 	}
 }
