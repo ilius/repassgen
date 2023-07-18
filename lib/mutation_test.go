f80df1e8a7ad2b64e1346fadfc138b322b18857f
Saeed Rasooli <saeed.gnu@gmail.com>
2023-07-19 00:58:32 +0330

    skip mutation_test.go on windows

--- lib/mutation_test.go
+++ lib/mutation_test.go
@@ -1,11 +1,17 @@
 package passgen_test
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	"github.com/gtramontina/ooze"
 )
 
 func TestMutation(t *testing.T) {
+	if os.PathSeparator == '\\' {
+		log.Println("TestMutation skipped because ooze does not support windows")
+		return
+	}
 	ooze.Release(t)
 }
